6563f890dc85944621414b819910c66cb0d8f8d4
ricsaittan <151973832+ricsaittan@users.noreply.github.com>
2024-02-12 19:01:02 -0700

    Lab4 Files uploaded

---
+++ Lab4/Lab4/Data/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace BlazorHybridApp.Data;
+
+public class TodoItem
+{
+    public string? Title {get; set;}
+    public bool IsDone {get; set;} = false;
+}
---
+++ Lab4/Lab4/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab4;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
